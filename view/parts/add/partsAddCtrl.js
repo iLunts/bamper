3b8f82c56069d478cad1d76e22fe97d65bff86bd
Roman Luntsevich <shipik2008@gmail.com>
2017-07-18 22:07:32 +0300

    Start working on page add new part.

---
+++ view/parts/add/partsAddCtrl.js
@@ -0,0 +1,11 @@
+app.config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.when("/parts/add", {
+            templateUrl: "view/parts/add/index.html",
+            controller: "partsAddCtrl"
+        });
+    }
+]);
+
+app.controller('partsAddCtrl', function($scope){
+
+});
