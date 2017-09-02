5638fad5d5ab8ff38e76a03783f5d3250f775de1
Roman Luntsevich <shipik2008@gmail.com>
2017-09-02 19:18:31 +0300

    added js for .form-group

--- view/parts/add/partsAddCtrl.js
+++ view/parts/add/partsAddCtrl.js
@@ -13,3 +13,39 @@ app.controller('partsAddCtrl', function($scope){
     $scope.showAdvacePhone = false;
 
 });
+
+// $(window).load(function () {
+//     $(".form-group .input").val("");
+
+//     $(".form-group .input").focusout(function () {
+//         if ($(this).val() != "") {
+//             $(this).addClass("isFull");
+//         } else {
+//             $(this).removeClass("isFull");
+//         }
+//     })
+// });
+
+
+(function () {
+    var materialForm;
+
+    materialForm = function () {
+        return $('.input').focus(function () {
+            return $(this).closest('.form-group').addClass('focused has-value');
+        }).focusout(function () {
+            return $(this).closest('.form-group').removeClass('focused');
+        }).blur(function () {
+            if (!this.value) {
+                $(this).closest('.form-group').removeClass('has-value');
+            }
+            return $(this).closest('.form-group').removeClass('focused');
+        });
+    };
+
+    $(function () {
+        return materialForm();
+    });
+
+}).call(this);
+
