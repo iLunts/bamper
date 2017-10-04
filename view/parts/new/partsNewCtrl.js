825648c9d8e0df55c3fb07d9c8ab4367852d98e3
Roman Luntsevich <shipik2008@gmail.com>
2017-10-04 13:53:52 +0300

    Create filter offcanvas

--- view/parts/new/partsNewCtrl.js
+++ view/parts/new/partsNewCtrl.js
@@ -177,4 +177,17 @@ app.controller('partsNewCtrl', function ($scope) {
         }
     ];
 
+
+
+    $('.input').on('blur', function () {
+        if ($(this).val().length === 0) {
+            $(this).closest('.form-group').removeClass('focused has-value').addClass('empty');
+        }
+        else {
+            $(this).closest('.form-group').removeClass('focused empty').addClass('has-value');
+        }
+    }).on('focus', function () {
+        $(this).closest('.form-group').removeClass('has-value empty').addClass('focused');
+    });
+
 });
--- web-src/js/app.js
+++ web-src/js/app.js
@@ -29,3 +29,4 @@ app.config(["$routeProvider", function($routeProvider) {
 }]);
 
 
+
